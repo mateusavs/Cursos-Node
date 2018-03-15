a6dbba85f5155448e9c8f7ae6212e977cb95bb6f
MATEUS-PC\Mateus <mateus.artur1597@gmail.com>
2018-03-15 15:59:35 -0300

    Projeto Node

--- ncursos/models/cursos.js
+++ ncursos/models/cursos.js
@@ -2,9 +2,9 @@ module.exports = function (app) {
     var mongoose = require('mongoose');
     var Schema = mongoose.Schema;
     var curso = Schema({
-        codigo: { type: String, required: true },
+        codigo: { type: Number, required: true },
         descricao: { type: String, required: true },
-        ch: { type: String, required: true },
+        ch: { type: Number, required: true },
         categoria: { type: String, required: true }
     });
     return mongoose.model('cursos', curso);
